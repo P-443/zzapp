266a573f298ec68b2f6a4ff9349f3f128eae1535
ᔆ ᴾ ᴱ ᴱ ᴰ ™ ⌁ 🇵🇸 <roroxroot@gmail.com>
2025-12-21 18:18:35 +0200

    Update testdb.js

--- testdb.js
+++ testdb.js
@@ -1,7 +1,7 @@
 const { Client } = require('pg');
 
 const client = new Client({
-  connectionString: 'postgres://postgres:PASSWORD@HOST:5432/postgres?sslmode=disable',
+  connectionString: 'postgres://postgres:V8CKJiFGGof7BrQWjRc1Ytgv0bIuN6vTaeCLyearfmEeUJiO1igU5WurO6v24nDs@zkwowwc8sk4kko8cksogcw80:5432/postgres?sslmode=require',
   ssl: false
 });
 
