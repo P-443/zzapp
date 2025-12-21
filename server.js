7872993670ebbc6b65042bf03a2504758df8f676
ᔆ ᴾ ᴱ ᴱ ᴰ ™ ⌁ 🇵🇸 <roroxroot@gmail.com>
2025-12-21 18:43:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ const upload = multer({
 
 /* ================= DATABASE ================= */
 // استخدام قاعدة البيانات البعيدة
-const connectionString = "postgres://postgres:Y7UfZjKmUsENlPibkeQv28p6TteDm7Xl2BPmWxKBIhlktc7TQeHTxXvbpzjxgKEJ@31.97.47.20:5433/postgres";
+const connectionString = "postgres://postgres:6DQNh71sjOwHWwi5VYvGGZDtx5GpsdXRz6DWQKb7mBy9fwHNTn9X21yAJy05A14v@31.97.47.20:5433/postgres?sslmode=allow";
 
 const pool = new Pool({
   connectionString: connectionString,
